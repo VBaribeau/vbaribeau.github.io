0b68550714b3e67e7f54cde81583908e566717e0
VBaribeau <vbaribea@monusb.ca>
2017-10-10 15:27:00 -0500

    as

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -1,8 +1,22 @@
-function setup() {
- createCanvas(windowWidth,windowHeight); 
+
+document.ontouchmove = function(event){
+ event.preventDefault();
 }
 
-function draw() {
+function setup() {
+ createCanvas(windowWidth,windowHeight);
+ rectMode(CENTER);
+ stroke(0);
+ noFill();
+ strokeWeight(5);
+}
+
+function draw(){
+ background(255);
+ 
+ for(var i=0;i<touches.length;i++){
+  rect(touches[i].x,touches[i].y,150,150);
+  }
 }
 
 function touchStarted() {
