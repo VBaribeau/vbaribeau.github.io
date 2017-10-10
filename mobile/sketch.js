0193a2036a3986f3dbd34cc5dc08669f71d66cdd
VBaribeau <vbaribea@monusb.ca>
2017-10-10 15:32:11 -0500

    a

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -1,29 +1,27 @@
-document.ontouchmove = function(event) {
-	event.preventDefault();
+
+document.ontouchmove = function(event){
+ event.preventDefault();
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	rectMode(CENTER);
-	stroke(0);
-	noFill();
-	strokeWeight(5);
+ createCanvas(windowWidth,windowHeight);
+ rectMode(CENTER);
+ stroke(0);
+ noFill();
+ strokeWeight(5);
 }
 
-function draw() {
-	background(255);
-	textSize(40);
-	text("Rx: " + rotationX, 100, 100);
-	text("Ry: " + rotationY, 100, 150);
-	text("Rz: " + rotationZ, 100, 200);
-	for (var i = 0; i < touches.length; i++) {
-		rect(touches[i].x, touches[i].y, 150, 150);
-	}
+function draw(){
+ background(255);
+ 
+ for(var i=0;i<touches.length;i++){
+  rect(touches[i].x,touches[i].y,150,150);
+  }
 }
 
 function touchStarted() {
-	background(255);
-	textSize(40);
-	text(touches[0].x, 100, 100);
-	text(touches[0].y, 200, 100);
+ background(255);
+ textSize(40);
+ text(touches[0].x, 100, 100);
+ text(touches[0].y, 200, 100);
 }
