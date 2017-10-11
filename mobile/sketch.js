32ddd0f4f67b0d2ddf907e92e1c7a0021fd136d9
VBaribeau <vbaribea@monusb.ca>
2017-10-11 09:31:47 -0500

    d

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -1,29 +1,31 @@
-
-document.ontouchmove = function(event){
- event.preventDefault();
+document.ontouchmove = function(event) {
+	event.preventDefault();
 }
 
 function setup() {
- createCanvas(windowWidth,windowHeight);
- rectMode(CENTER);
- stroke(0);
- fill(100,255,100);
- strokeWeight(5);
+	createCanvas(windowWidth, windowHeight);
+	rectMode(CENTER);
+	stroke(0);
+	strokeWeight(5);
 }
 
-function draw(){
- background(255);
- beginShape();
- for(var i=0;i<touches.length;i++){
-  ellipse(touches[i].x,touches[i].y,75,75);
-  vertex(touches[i].x,touches[i].y);
-  }
- endShape(CLOSE);
+function draw() {
+	background(255);
+	fill(175, 255, 100);
+	for (var i = 0; i < touches.length; i++) {
+		ellipse(touches[i].x, touches[i].y, 75, 75);
+	}
+	fill(100, 200, 100);
+	beginShape();
+	for (var i = 0; i < touches.length; i++) {
+		vertex(touches[i].x, touches[i].y);
+	}
+	endShape(CLOSE);
 }
 
 function touchStarted() {
- background(255);
- textSize(40);
- text(touches[0].x, 100, 100);
- text(touches[0].y, 200, 100);
+	background(255);
+	textSize(40);
+	text(touches[0].x, 100, 100);
+	text(touches[0].y, 200, 100);
 }
