68548cb283d61545faac73ce7dd21d58edd56d59
VBaribeau <vbaribea@monusb.ca>
2017-10-11 10:32:53 -0500

    a

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -14,16 +14,20 @@ function draw() {
 	background(255);
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
-		fill(0);
-		textSize(25);
-		text(touches[i].x, touches[i].x, touches[i].y);
-		text(touches[i].y, touches[i].x + 100, touches[i].y);
 		fill(100, 200, 100);
 		vertex(touches[i].x, touches[i].y);
 	}
 	endShape(CLOSE);
 	fill(175, 255, 100);
 	for (var j = 0; j < touches.length; j++) {
+		floor(touches[i].x);
+		floor(touches[i].y);
 		ellipse(touches[j].x, touches[j].y, 50, 50);
+		strokeWeight(1);
+		fill(0);
+		textSize(25);
+		text(touches[i].x, touches[i].x, touches[i].y);
+		text(touches[i].y, touches[i].x + 100, touches[i].y);
+
 	}
 }
