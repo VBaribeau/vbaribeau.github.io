b2afc819de6017f5e01df1a3bee85cc8df8efd80
VBaribeau <vbaribea@monusb.ca>
2017-10-11 09:51:05 -0500

    s

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -1,7 +1,6 @@
 document.ontouchmove = function(event) {
 	event.preventDefault();
 
-	var pos1, pos2;
 }
 
 function setup() {
@@ -9,8 +8,6 @@ function setup() {
 	rectMode(CENTER);
 	stroke(0);
 	strokeWeight(5);
-	pos1 = 100;
-	pos2 = 40;
 }
 
 function draw() {
@@ -18,10 +15,9 @@ function draw() {
 	fill(100, 200, 100);
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
-		pos1 = pos1 + pos2;
 		textSize(40);
-		text(touches[i].x, 100, pos1);
-		text(touches[i].y, 200, pos1);
+		text(touches[i].x, 100, 100);
+		text(touches[i].y, 200, 100);
 		vertex(touches[i].x, touches[i].y);
 	}
 	endShape(CLOSE);
