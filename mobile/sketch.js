250492651336b39680231e9a92486c8a15488128
VBaribeau <vbaribea@monusb.ca>
2017-10-11 09:35:21 -0500

    a

--- mobile/sketch.js
+++ mobile/sketch.js
@@ -11,16 +11,16 @@ function setup() {
 
 function draw() {
 	background(255);
-	fill(175, 255, 100);
-	for (var i = 0; i < touches.length; i++) {
-		ellipse(touches[i].x, touches[i].y, 75, 75);
-	}
 	fill(100, 200, 100);
 	beginShape();
 	for (var i = 0; i < touches.length; i++) {
 		vertex(touches[i].x, touches[i].y);
 	}
 	endShape(CLOSE);
+	fill(175, 255, 100);
+	for (var j = 0; j < touches.length; j++) {
+		ellipse(touches[j].x, touches[j].y, 75, 75);
+	}
 }
 
 function touchStarted() {
