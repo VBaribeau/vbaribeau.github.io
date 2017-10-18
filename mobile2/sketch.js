9b82cb3fea3d12e0c9829a13e1e097eefe622017
VBaribeau <vbaribea@monusb.ca>
2017-10-18 08:56:30 -0500

    a

--- mobile2/sketch.js
+++ mobile2/sketch.js
@@ -1,11 +1,26 @@
+var ax, ay, vx, vy, px, py, vMultiplier;
+
 function setup() {
- createCanvas(windowWidth,windowHeight); 
+	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
- background(255);
- textSize(40);
- text("Rx: " + rotationX, 100, 100);
- text("Ry: " + rotationY, 100, 150);
- text("Rz: " + rotationZ, 100, 200);
+	background(255);
+	textSize(40);
+	text("Rx: " + rotationX, 100, 100);
+	text("Ry: " + rotationY, 100, 150);
+	text("Rz: " + rotationZ, 100, 200);
+	Marble();
+}
+
+function Marble() {
+	fill(150, 150, 150);
+	vMultiplier = 0.01;
+	ax = rotationY x vMultiplier;
+	vx += ax;
+	px += vx;
+	ay = rotationX x vMultiplier;
+	vy += ay;
+	py += vy;
+	ellipse(px, py, 100, 100);
 }
