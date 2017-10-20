7be835eb7bbdb5b75c143d082499ee50869d95c9
VBaribeau <vbaribea@monusb.ca>
2017-10-19 19:04:32 -0500

    a

--- mobile2/sketch.js
+++ mobile2/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 	s1 = 60;
 	s2 = s1 / 6.66;
 	s3 = s1 / 4.2;
-	g = 0.5;
+	g = 0.7;
 	r = s1 / 2;
 	b = 0;
 	w = 255;
@@ -22,7 +22,7 @@ function draw() {
 	background(b);
 	fill(w);
 	textSize(15);
-	text("Test2.3", 10, 15);
+	text("Final", 10, 15);
 	text("Rx: " + floor(rotationX), 10, 30);
 	text("Ry: " + floor(rotationY), 10, 45);
 	Marble();
