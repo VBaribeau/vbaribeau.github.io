76283e2ae40844b0156acac159ac6351baf53b9c
VBaribeau <vbaribea@monusb.ca>
2017-10-24 13:32:03 -0500

    Final

--- mobile2/sketch.js
+++ mobile2/sketch.js
@@ -2,10 +2,9 @@
 var stars = [];
 var maxStars = 50;
 //***************************************************
-
 var s1, ax, ay, vx, vy, px, py;
-var b, w, vMultiplier, g, r, u, d, check1, check2;
-
+var b, w, vMultiplier, g, r;
+//***************************************************
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	noStroke();
@@ -22,33 +21,26 @@ function setup() {
 	r = s1 / 2;
 	b = 1;
 	w = 254;
-	u = 1;
-	d = 1;
-	check1 = false;
-	check2 = false;
 	for (var i = 0; i < maxStars; i++) {
-		stars[i] = new Star;
+		stars[i] = new Star();
 	}
-
 }
-
+//***************************************************
 function draw() {
 	background(b);
-	Ifs();
 	//***************************************************
 	for (var i = 0; i < maxStars; i++) {
 		drawStar(stars[i].X, stars[i].Y, stars[i].size);
 	}
-
 	//***************************************************
 	fill(w);
 	textSize(15);
-	text("-!Final!-", 10, 15);
+	text("Finale", 10, 15);
 	text("Rx: " + floor(rotationX), 10, 30);
 	text("Ry: " + floor(rotationY), 10, 45);
 	Marble();
 }
-
+//***************************************************
 function Marble() {
 	vMultiplier = 0.01;
 	ax = rotationY * vMultiplier;
@@ -60,60 +52,35 @@ function Marble() {
 	fill(0, 201, 255);
 	ellipse(px, py, s1, s1);
 	fill(0, 255, 128);
-	ellipse(px - s2, py - s2, s1 / 2, s1 / 2);
+	ellipse(px - s2, py - s2, r, r);
 	fill(33, 190, 111);
-	ellipse(px + s3, py + s3, s1 / 4, s1 / 4);
-	if (px > windowWidth - s1 / 2) {
-		px = windowWidth - s1 / 2;
+	ellipse(px + s3, py + s3, s1 / 4, r / 2);
+	if (px > windowWidth - r) {
+		px = windowWidth - r;
 		vx = -vx * g;
 	}
-	if (px < 0 + s1 / 2) {
-		px = 0 + s1 / 2;
+	if (px < 0 + r) {
+		px = 0 + r;
 		vx = -vx * g;
 	}
-	if (py > windowHeight - s1 / 2) {
-		py = windowHeight - s1 / 2;
+	if (py > windowHeight - r) {
+		py = windowHeight - r;
 		vy = -vy * g;
 	}
-	if (py < 0 + s1 / 2) {
-		py = 0 + s1 / 2;
+	if (py < 0 + r) {
+		py = 0 + r;
 		vy = -vy * g;
 	}
 }
-
-
-
-
 //***************************************************
 function deviceShaken() {
-	b = b + u;
+	b = b + 1;
 	if (b >= 255) {
-		check1 === true;
+		b = 0;
 	}
-	if (b <= 0) {
-		check1 === false;
-	}
-	w = w + d;
+	w = w - 1;
 	if (w <= 0) {
-		check2 === true;
-	}
-	if (w >= 255) {
-		check2 === false;
-	}
-}
-//***************************************************
-function Ifs() {
-	if (check1 === true) {
-		u = -1;
-	}
-	if (check2 === true) {
-		d = -1;
-	}
-	if (check1 === false) {
-		u = 1;
-	}
-	if (check2 === false) {
-		d = 1;
+		w = 255
 	}
 }
 //***************************************************
@@ -129,7 +96,6 @@ function Star() {
 //*******************************
 function drawStar(x, y, s) {
 	fill(w);
-	x = x++;
 	ellipse(x, y, s, s);
 }
 //******************************************************************************
